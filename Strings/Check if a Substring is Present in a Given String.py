83615de20b692fe314579e5110f74ffc951075c5
M.V.S.Sai hiranmayee <62197337+hiranmayee1123@users.noreply.github.com>
2021-02-13 19:32:46 +0530

    Add files via upload

---
+++ Strings/Check if a Substring is Present in a Given String.py
@@ -0,0 +1,10 @@
+def check(string, sub_str): 
+    if (string.find(sub_str) == -1): 
+        print("NO") 
+    else: 
+        print("YES") 
+            
+# driver code 
+string = "geeks for geeks"
+sub_str ="geek"
+check(string, sub_str) 
